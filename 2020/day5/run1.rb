2d3d9d603e3932768bdd59b2ad60f9a70a4cb21c
Dylan Bridgman <light@ether.org.za>
2021-02-08 17:19:29 +0200

    2020 - day 5

---
+++ 2020/day5/run1.rb
@@ -0,0 +1,28 @@
+load "./data.rb"
+#load "./test.rb"
+
+def mid(range)
+  ((range.min + range.max) / 2)
+end
+
+@data = @data.split(/\n+/).map do |seat|
+  seat.split("").reduce([0..127,0..7]) do |ranges, char|
+    case char
+    when "F"
+      ranges[0] = ranges[0].min..mid(ranges[0]).floor
+    when "B"
+      ranges[0] = mid(ranges[0]).ceil..ranges[0].max
+    when "L"
+      ranges[1] = ranges[1].min..mid(ranges[1]).floor
+    when "R"
+      ranges[1] = mid(ranges[1]).ceil..ranges[1].max
+    end
+    ranges
+  end.map do |range|
+    range.max
+  end
+end.map do |seat|
+  seat[0] * 8 + seat[1]
+end.max
+
+print "#{@data}\n"
